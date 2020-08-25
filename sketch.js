940d4c530635f94fe2ceeffe519cef49967c9d33
Sneha Nandigala <snehanandigala@gmail.com>
2020-08-25 21:16:45 +0530

    add platform

--- sketch.js
+++ sketch.js
@@ -15,6 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+
     
     ground = new Ground(600,height,1200,20)
 
@@ -34,7 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+platform=new Ground(150,305,300,170)
 }
 
 function draw(){
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+platform.display
 }
